0095819d4c43c82497972dfbd0ee30d1801b0b10
Sthiam915 <sthiam915@gmail.com>
2025-11-19 16:20:56 -0500

    added requirements and fixed up backend

--- src/backend/inference/general_bounding_box.py
+++ src/backend/inference/general_bounding_box.py
@@ -1,18 +1,25 @@
 # standard lib imports
-
-# third party imports
-from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 import io
 
+# third party imports
+from ultralytics import YOLO
+from ultralytics.engine.results import Results 
 # project imports
 from inference import model_paths
 
 model = YOLO(model_paths.yolov9e)
 
 def draw_box(image:Image, model:YOLO=model) -> bytes:
-    result = model(image)
+    result:Results = model(image)[0]
+    
+    boxes = result.boxes.xyxy
+    cls = result.boxes.cls
+    mask = cls == 14
+    
+    boxes = boxes[mask]
+    
     image_with_boxes = result[0].plot(labels=False)
     img_pil = Image.fromarray(image_with_boxes)
     r, g, b = img_pil.split()
